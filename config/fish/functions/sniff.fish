a48883b2ed7f2bbdf8c3aa40c15a0727bcf6a965
Salvador Jacobi <salvador.jacobi@gmail.com>
2014-11-30 01:37:48 +0100

    add sniff fish function to capture with airportd and view with wireshark

---
+++ config/fish/functions/sniff.fish
@@ -0,0 +1,27 @@
+function sniff --description "capture 802.11 frames with airportd and view with wireshark"
+	set -l interface en0
+	if test (count $argv) -ge 1
+		set interface $argv[0]
+	end
+
+	find /tmp/ -name "airportSniff*.cap" -delete # delete previous captures
+
+	if test (count $argv) -ge 2
+		set -l channel $argv[1]
+		/usr/libexec/airportd $interface sniff $channel & # start capture on specific channel
+	else
+		/usr/libexec/airportd $interface sniff & # start capture current channel
+	end
+
+	sleep 1
+
+	set -l capture (find /tmp/ -name "airportSniff*.cap") # find current capture
+
+	if test (count $capture) -eq 1
+		tail -f -n +1 $capture | cat -u | wireshark -k -i - # start wireshark
+	else
+		echo 'current capture not found'
+	end
+
+	killall airportd # stop capture when wireshark terminates
+end

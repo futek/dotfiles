21afab9fd961aaa2de839fb1da7c9a4a5f30cee6
Salvador Jacobi <salvador.jacobi@gmail.com>
2015-01-09 21:24:03 +0100

    fix sniff parameters

--- config/fish/functions/sniff.fish
+++ config/fish/functions/sniff.fish
@@ -1,13 +1,13 @@
 function sniff --description "capture 802.11 frames with airportd and view with wireshark"
 	set -l interface en0
 	if test (count $argv) -ge 1
-		set interface $argv[0]
+		set interface $argv[1]
 	end
 
 	find /tmp/ -name "airportSniff*.cap" -delete # delete previous captures
 
 	if test (count $argv) -ge 2
-		set -l channel $argv[1]
+		set -l channel $argv[2]
 		/usr/libexec/airportd $interface sniff $channel & # start capture on specific channel
 	else
 		/usr/libexec/airportd $interface sniff & # start capture current channel
